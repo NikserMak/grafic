2c39dc515d50e6cad2455965de72e706ddd12368
Sergeev Nikita <69539697+NikserMak@users.noreply.github.com>
2025-10-09 14:27:56 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'schedule-v1.3';
+const urlsToCache = [
+  '/',
+  '/styles.css',
+  '/app.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
